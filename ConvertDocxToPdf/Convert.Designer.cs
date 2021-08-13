7b76f792f962efb46625c8081f5ee33cf0d44a5e
DESKTOP-84OTFQ1\VH-HP <vanhung3693@gmail.com>
2021-08-13 17:32:31 +0700

    Add project files.

---
+++ ConvertDocxToPdf/Convert.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace ConvertDocxToPdf
+{
+    partial class Convert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnChooseIn = new System.Windows.Forms.Button();
+            this.btnChooseOut = new System.Windows.Forms.Button();
+            this.txtOutPut = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(188, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Convert Docx sang PDF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(186, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Convert Docx sang PDF";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Thư mục chứa file docx:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(141, 43);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(324, 20);
+            this.txtInput.TabIndex = 2;
+            // 
+            // btnChooseIn
+            // 
+            this.btnChooseIn.Location = new System.Drawing.Point(464, 42);
+            this.btnChooseIn.Name = "btnChooseIn";
+            this.btnChooseIn.Size = new System.Drawing.Size(75, 23);
+            this.btnChooseIn.TabIndex = 3;
+            this.btnChooseIn.Text = "Chọn";
+            this.btnChooseIn.UseVisualStyleBackColor = true;
+            this.btnChooseIn.Click += new System.EventHandler(this.btnChooseIn_Click);
+            // 
+            // btnChooseOut
+            // 
+            this.btnChooseOut.Location = new System.Drawing.Point(464, 74);
+            this.btnChooseOut.Name = "btnChooseOut";
+            this.btnChooseOut.Size = new System.Drawing.Size(75, 23);
+            this.btnChooseOut.TabIndex = 6;
+            this.btnChooseOut.Text = "Chọn";
+            this.btnChooseOut.UseVisualStyleBackColor = true;
+            this.btnChooseOut.Click += new System.EventHandler(this.btnChooseOut_Click);
+            // 
+            // txtOutPut
+            // 
+            this.txtOutPut.Location = new System.Drawing.Point(141, 75);
+            this.txtOutPut.Name = "txtOutPut";
+            this.txtOutPut.Size = new System.Drawing.Size(324, 20);
+            this.txtOutPut.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Thư mục lưu file pdf:";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(237, 101);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 7;
+            this.btnConvert.Text = "Bắt Đầu";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Convert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 170);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnChooseOut);
+            this.Controls.Add(this.txtOutPut);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnChooseIn);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Convert";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Convert";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnChooseIn;
+        private System.Windows.Forms.Button btnChooseOut;
+        private System.Windows.Forms.TextBox txtOutPut;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
---
+++ ConvertDocxToPdf/Convert.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Office.Core;
+using Microsoft.Office.Interop.Word;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using Application = Microsoft.Office.Interop.Word.Application;
+
+namespace ConvertDocxToPdf
+{
+    public partial class Convert : Form
+    {
+        public Convert()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChooseIn_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath, "*.docx");
+                    if (files.Length > 0)
+                    {
+                        System.Windows.Forms.MessageBox.Show("Tìm Thấy : " + files.Length.ToString() + " file .docx", "Thông Báo");
+                        txtInput.Text = fbd.SelectedPath;
+                    }
+                    else
+                    {
+                        System.Windows.Forms.MessageBox.Show("Tìm Thấy : " + files.Length.ToString() + " file .docx cần chọn lại thư mục chứa file", "Thông Báo");
+                    }
+                }
+            }
+        }
+
+        private void btnChooseOut_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    txtOutPut.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+
+            if(txtInput.Text != "" && txtOutPut.Text !="")
+            {
+                btnConvert.Enabled = false;
+                CreatePDF(txtInput.Text, txtOutPut.Text);
+                btnConvert.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Bạn chưa chọn thư mục chứa file docx hoặc lưu file","Thông Báo");
+            }
+
+        }
+
+        private bool CreatePDF(string path, string exportDir)
+        {
+            Application app = new Application();
+            app.DisplayAlerts = WdAlertLevel.wdAlertsNone;
+            app.Visible = false;
+
+            string[] files = System.IO.Directory.GetFiles(path, "*.docx");
+
+            foreach (var file in files)
+            {
+                var objPresSet = app.Documents;
+                var objPres = objPresSet.Open(file, MsoTriState.msoTrue, MsoTriState.msoTrue, MsoTriState.msoFalse);
+
+                var pdfFileName = Path.ChangeExtension(file, ".pdf");
+
+                var pdfPath = Path.Combine(pdfFileName, exportDir + "\\" + Path.GetFileName(pdfFileName));
+
+                try
+                {
+                    objPres.ExportAsFixedFormat(
+                        pdfPath,
+                        WdExportFormat.wdExportFormatPDF,
+                        false,
+                        WdExportOptimizeFor.wdExportOptimizeForPrint,
+                        WdExportRange.wdExportAllDocument
+                    );
+                }
+                catch
+                {
+                    return false;
+                }
+                finally
+                {
+                    objPres.Close();
+                }
+            }
+
+            return true;
+        }
+    }
+}
